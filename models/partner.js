964227a923a9648d3a7fe9dd09659c54c50b4b51
anvil-ideation <landon@anvilideation.com>
2021-02-06 08:34:45 -0700

    Week 2 Workshop Assignment

---
+++ models/partner.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+
+const partnersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+const Partner = mongoose.model('Partner', partnersSchema);
+
+module.exports = Partner;
