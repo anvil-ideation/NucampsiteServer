6b8f9528bc603743dc1ffed90a0fa144d7c6385c
anvil-ideation <landon@anvilideation.com>
2021-02-15 12:38:37 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const Facebook = require('./hidden');
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-}
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: Facebook.clientId,
+        clientSecret: Facebook.clientSecret,
+    }
+};
